3014d0eceb960d58c5f99217f2fa00973e47441d
GabrielWeimer <141836588+GabrielWeimer@users.noreply.github.com>
2023-10-11 14:14:56 -0300

    Update cinco.ts

--- Aula1/cinco.ts
+++ Aula1/cinco.ts
@@ -1,11 +1,13 @@
+import * as leitor from "readline-sync"
+
 function main () {
     let numero1: Number
     let numero2: Number
     let numero3: Number
 
-    numero1 = 5
-    numero2 = 3
-    numero3 = 10
+    numero1 = leitor.questionFloat("Digite o primeiro numero:")
+    numero2 = leitor.questionFloat("Digite o segundo numero:)
+    numero3 = leitor.questionFloat("Digite o terceiro numero:")
 
     console.log("Soma:" ,somar(numero1, numero2, numero3));
     console.log("Subtração:" ,subtrair(numero1, numero2, numero3));
