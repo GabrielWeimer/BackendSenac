f0fc5bab22f415375fda59e65386707b6ad4e364
GabrielWeimer <141836588+GabrielWeimer@users.noreply.github.com>
2023-10-17 16:02:46 -0300

    Add files via upload

---
+++ Aula3/Interpretacao.ts
@@ -0,0 +1,57 @@
+/*
+1:
+While(loop){
+    enquanto o loop for true
+    o codigo dentro das chaves vai ficar sendo executado
+    quando a condicao ficar false ele para
+}
+
+2: podemos colocar o switch case dentro do while para fazer um loop de verificaçoes e uma das opçoes pode fazer a variavel do while ficar falsa e parar o loop
+
+3: definindo uma situaçao pra cada caso 
+
+4: nao 
+
+5: sim
+
+6: é possivel ter um caso padrao que é o default
+
+7: caso nenhuma das verificacoes seja atendida e existir um default ele sera executado, se nao o switch só nao vai ser executado
+
+8:
+switch (comidaFavorita){`
+case 'pastel':
+    console.log("Sua comida favorita é pastel")
+break
+case 'acai':
+    console.log("Sua comida favorita é acai")
+break
+case 'lasanha':
+    console.log("Sua comida favorita é lasanha")
+break
+}
+
+9: sim é pode ser ultilizada no default 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ Aula3/Pratica.ts
@@ -0,0 +1,230 @@
+import * as leitor from "readline-sync"
+
+/*
+Questão 1:
+let continuar = true
+while(continuar){
+    let pergunta: Number = leitor.questionInt("Digite 1 2 ou 3:")
+    switch(pergunta){
+        case 1:
+            console.log("Oi")
+            break
+        case 2:
+            console.log("eae")
+            break
+        case 3:
+            console.log("tchau")
+            continuar = false
+            break
+        default:
+            console.log("Resposta nao encontrada")
+        break
+    }
+
+}
+*/
+
+/*
+Questão 2:
+let continuar = true
+while(continuar){
+    let pergunta: String = leitor.question("Digite uma cor que voce prefere entre azul, vermelho e amarelo:").toLowerCase()
+    switch(pergunta){
+        case "azul":
+            console.log("Azul me lembra o mar os smurfs e o gremio.")
+            break
+        case "vermelho":
+            console.log("Vermelho me lembra o vascao da gama ")
+            break
+        case "amarelo":
+            console.log("Amarelo me lembra luz, estrelas e a noite.")
+            continuar = false
+            break
+        default:
+            console.log("Resposta nao encontrada")
+        break
+    }
+
+}
+
+*/
+
+/*
+Questão 3:
+
+let continuar = true
+while(continuar){
+    let pergunta: Number = leitor.questionInt("Digite digite o numero de 1 a 12 para saber o mês:")
+    switch(pergunta){
+        case 1:
+            console.log("O mês 1 é janeiro.")
+            break
+        case 2:
+            console.log("O mês 2 é fevereiro.")
+            break
+        case 3:
+            console.log("O mês 3 é março.")
+            break
+        case 4:
+            console.log("O mês 4 é abril.")
+            break
+        case 5:
+            console.log("O mês 5 é maio.")
+            break
+        case 6:
+            console.log("O mês 6 é junho.")
+            break
+        case 7:
+            console.log("O mês 7 é julho.")
+            break
+        case 8:
+            console.log("o mês 8 é agosto.")
+            break
+        case 9:
+            console.log("O mês 9 é setembro.")
+            break
+        case 10:
+            console.log("O mês 10 é outubro.")
+            break
+        case 11:
+            console.log("O mês 11 é novembro.")
+            break
+        case 12:
+            console.log("O mês 12 é dezembro.")
+            continuar = false
+            break
+        default:
+            console.log("Opção não encontrada")
+        break
+    }
+
+}
+
+*/
+
+/*
+Questao 4:
+let continuar = true
+while(continuar){
+    let pergunta: String = leitor.question("Digite para saber o preço A)Barra cereal B)Pirulito ou C)Chiclete:").toLowerCase()
+    switch(pergunta){
+        case "a":
+            console.log("R$:3,00")
+            break
+        case "b":
+            console.log("R$:0,25")
+            break
+        case "c":
+            console.log("R$:0,50")
+            continuar = false
+            break
+        default:
+            console.log("Resposta nao encontrada")
+        break
+    }
+
+}
+*/
+
+
+/*
+Questao 5
+let continuar = true
+while(continuar){
+    let teste = true
+    let pergunta = leitor.questionInt("Informe uma nota de 0 a 100 ou 101 para sair: ")
+    switch(teste){
+        case (pergunta <= 20):
+            console.log("Seu conceito é F :(");
+            break;
+        case (pergunta <= 40):
+            console.log("Seu conceito é D :/");
+            break;
+        case (pergunta <= 60):
+            console.log("Seu conceito é C :|");
+            break;
+        case (pergunta <= 80):
+            console.log("Seu conceito é B :)");
+            break;
+        case (pergunta <= 100):
+            console.log("Seu conceito é A :D");
+            break;
+        case (pergunta === 101):
+            console.log("Saindo...");
+            continuar = false;
+            break;
+    }
+}
+*/
+
+
+/*
+Questao 6:
+let continuar = true
+while(continuar){
+    let pergunta: String = leitor.question("Para saber sua velocidade media digite \ncarro \nonibus \nbicicleta \nsair \n").toLowerCase()
+    switch(pergunta){
+        case "carro":
+            console.log("Carro 80Km/h")
+            break
+        case "onibus":
+            console.log("Ônibus 50km/h")
+            break
+        case "bicicleta":
+            console.log("Bicicleta 20km/h")
+            break
+        case "sair":
+            continuar = false
+            break
+        default:
+            console.log("Resposta nao encontrada")
+        break
+    }
+
+}
+*/
+
+/*
+let continuar = true
+while(continuar){
+    let pergunta: Number = leitor.questionInt("Digite um numero de 1 a 5 ou 7 para sair:")
+    switch(pergunta){
+        case 1:
+            console.log("Voce digitou 1.")
+            break
+        case 2:
+            console.log("Voce digitou 2.")
+            break
+        case 3:
+            console.log("Voce digitou 3.")
+            break
+        case 4:
+            console.log("Voce digitou 4.")
+            break
+        case 5: 
+            console.log("Voce digitou 5.")
+            break
+        case 6:
+            console.log("Voce digitou o numero secreto :D !!!");
+            break
+        case 7:
+            continuar = false
+            break
+        default:
+            console.log("Resposta nao encontrada")
+        break
+    }
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
